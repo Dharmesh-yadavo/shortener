da7729739b7cff128968b9ac5bba9b84dc9ad848
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-02-20 16:30:23 +0530

    update in code

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,11 +1,7 @@
 // lib/stripe.ts
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SECRET_KEY is missing from .env.local");
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   // Use the latest API version or a specific one compatible with your project
   apiVersion: "2026-01-28.clover",
   typescript: true,
